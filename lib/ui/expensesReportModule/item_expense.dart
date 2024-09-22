3c836226b3f4dd532083c83524e93a3d18894b39
Jonatan Morales <johnta2610@hotmail.com>
2024-09-21 22:02:10 -0600

    � Fix: Resolve error with expense amount is true or false

--- lib/ui/expensesReportModule/item_expense.dart
+++ lib/ui/expensesReportModule/item_expense.dart
@@ -5,30 +5,39 @@ class ItemExpense extends StatelessWidget {
   final ExpenseEntity expenseEntity;
   final VoidCallback onDelete;
 
-  const ItemExpense(
-      {super.key, required this.expenseEntity, required this.onDelete});
+  const ItemExpense({
+    super.key,
+    required this.expenseEntity,
+    required this.onDelete,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(child: Text(expenseEntity.id.toString().trim())),
-          Expanded(child: Text(expenseEntity.typeExpense)),
-          Expanded(
-              child: Checkbox(
-                  onChanged: (value) {
-                    if (value != null) {
-                      expenseEntity.isBill = value;
-                    }
-                  },
-                  value: expenseEntity.isBill)),
-          Expanded(child: Text(expenseEntity.bill)),
-          Expanded(child: Text('${expenseEntity.amount}')),
-          Expanded(
-              child: IconButton(
-                  onPressed: () => onDelete, icon: const Icon(Icons.delete))),
-        ]);
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(child: Text(expenseEntity.id.toString().trim())),
+        Expanded(child: Text(expenseEntity.typeExpense)),
+        Expanded(
+          child: Checkbox(
+            onChanged: (value) {
+              if (value != null) {
+                expenseEntity.isBill = value;
+              }
+            },
+            value: expenseEntity.isBill,
+          ),
+        ),
+        Expanded(child: Text(expenseEntity.bill)),
+        Expanded(child: Text('${expenseEntity.amount}')),
+        Expanded(
+          child: IconButton(
+            onPressed: onDelete, // Aquí se llama a la función
+            icon: const Icon(Icons.delete),
+          ),
+        ),
+      ],
+    );
   }
 }
